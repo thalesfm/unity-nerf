d388802fb49b69da9e14137f0bdb0f8677efab27
Thales Magalhães <thls.mglhs@gmail.com>
2024-10-01 13:12:01 -0300

    Add test for conversion

--- Assets/Editor/N3Tree.cs
+++ Assets/Editor/N3Tree.cs
@@ -37,11 +37,11 @@ namespace UnityNeRF
             using var npz = new NpzFile(stream);
 
             tree.data_dim = (int)npz.ReadInt64("data_dim.npy");
-            Debug.Log($"data_dim = {tree.data_dim}");
+            // Debug.Log($"data_dim = {tree.data_dim}");
 
             tree.child = (NDArray<int>)npz.ReadArray<int>("child.npy", out int[] shape);
             tree.child = tree.child.reshape(shape);
-            Debug.Log($"child = {tree.child}");
+            // Debug.Log($"child = {tree.child}");
 
             tree.N = tree.child.shape[^1];
             // Assert N == 2
@@ -56,28 +56,49 @@ namespace UnityNeRF
                 float invradius = npz.ReadSingle("invradius.npy");
                 tree.invradius = new Vector3(invradius, invradius, invradius);
             }
-            Debug.Log($"invradius = {tree.invradius}");
+            // Debug.Log($"invradius = {tree.invradius}");
             
             float[] offset = npz.ReadArray<float>("offset.npy");
             tree.offset = new Vector3(offset[0], offset[1], offset[2]);
-            Debug.Log($"offset = {tree.offset}");
+            // Debug.Log($"offset = {tree.offset}");
 
             tree.depth_limit = (int)npz.ReadInt64("depth_limit.npy");
-            Debug.Log($"depth_limit = {tree.depth_limit}");
+            // Debug.Log($"depth_limit = {tree.depth_limit}");
 
             Half[] data = npz.ReadArray<Half>("data.npy", out shape);
             tree.data = (NDArray<float>)data.Select(value => (float)value).ToArray();
             tree.data = tree.data.reshape(shape);
-            Debug.Log($"data = {tree.data}");
+            // Debug.Log($"data = {tree.data}");
 
             // if (npz.ContainsEntry("data_format.npy"))
             tree.data_format = DataFormat.Parse(npz.ReadString("data_format.npy"));
-            Debug.Log($"data_format = {tree.data_format}");
+            // Debug.Log($"data_format = {tree.data_format}");
             
             if (npz.ContainsEntry("extra_data.npy"))
                 throw new NotSupportedException("Extra data found!");
             
             return tree;
+        }
+
+        public NDArray Sample(Vector3 coord)
+        {
+            coord = coord.Clamp(0.0f, 1.0f - 1e-10f);
+            int ptr = 0;
+
+            for (int i = 0; i <= depth_limit; ++i)
+            {
+                Vector3Int idx = (2.0f * coord).FloorToInt();
+                int skip = child[ptr, idx.x, idx.y, idx.z];
+                if (skip == 0)
+                {
+                    NDArray slice = ((NDArray) data)[ptr, idx.x, idx.y, idx.z, Slice.All];
+                    return slice;
+                }
+                coord = 2.0f * coord - idx;
+                ptr += skip;
+            }
+
+            throw new Exception();
         }
     }
 } // namespace UnityNeRF
--- Assets/Tests/Editor/TestN3Tree.cs
+++ Assets/Tests/Editor/TestN3Tree.cs
@@ -1,27 +1,49 @@
 using System;
 using NUnit.Framework;
 using NumSharp;
-using UnityNeRF.Editor.IO;
+using UnityEngine;
 
 namespace UnityNeRF.Editor.Tests
 {
     public class TestN3Tree
     {
-        [TestCase(@"Assets/Resources/oct_lego.npz")]
+        [TestCase(@"Assets/Resources/oct_lego.npz", Ignore = "Slow")]
         public static void Load_NoThrow(string path)
         {
             N3Tree tree = N3Tree.Load(path);
-            // UnityEngine.Debug.Log($"tree.depth_limit = {tree.depth_limit}");
-            // UnityEngine.Debug.Log($"tree.FinMaxLevel() = {tree.FindMaxLevel()}");
         }
 
-        [TestCase(@"Assets/Resources/oct_lego.npz")]
+        [TestCase(@"Assets/Resources/oct_lego.npz", Ignore = "Slow")]
         public static void ToSparseVoxelOctree_NoThrow(string path)
         {
             UnityEngine.Debug.Log($"Loading plenoctree...");
             N3Tree tree = N3Tree.Load(path);
             UnityEngine.Debug.Log($"Converting to octree...");
             SparseVoxelOctree<float[]> octree = tree.ToSparseVoxelOctree();
+        }
+
+        [TestCase(@"Assets/Resources/oct_lego.npz")] // , Explicit = true, Ignore = "Slow")]
+        public static void ToSparseVoxelOctree_Equal(string path, int samples = 20)
+        {
+            N3Tree tree = N3Tree.Load(path);
+            SparseVoxelOctree<float[]> octree = tree.ToSparseVoxelOctree();
+            System.Random random = new System.Random();
+
+            for (int k = 0; k < samples; ++k)
+            {
+                float x = (float) random.NextDouble();
+                float y = (float) random.NextDouble();
+                float z = (float) random.NextDouble();
+                NDArray expected = tree.Sample(new Vector3(x, y, z));
+
+                int xx = (int) x * octree.Width;
+                int yy = (int) y * octree.Height;
+                int zz = (int) z * octree.Depth;
+                float[] coeffs = octree[xx, yy, zz];
+
+                for (int i = 0; i < tree.data_dim; ++i)
+                    Assert.That(coeffs[i], Is.EqualTo(expected.GetSingle(i)));
+            }
         }
     }
 } // namespace UnityNeRF.Editor.Tests
